209f1bdb107ba247a52ae7dcf9e41e6425b88909
chiakihonda <chiaki.honnda@gmail.com>
2020-05-18 17:00:15 +0900

    learn store and setState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import PropTypes from "prop-types"
+import React, {Component } from 'react';
 
-const App = () =>{
-  const profiles =[
-    {name:"Taro",age:10},
-    {neme:"Hanako",age:5},
-    {name:"NoName"}
-  ]
-  return(
-    <div>
-      {
-        profiles.map((prifile,index) => {
-          return <User name={prifile.name} age={prifile.age} key={index} />
-        })
-      }
-    </div>
-  ) 
+const App = () =>(<Counter></Counter>)
+
+class Counter extends Component{
+  constructure(props){
+    super(props)
+    this.state={count:0}
+  }
+  handlePlusButton=()=>{
+    this.setState({ count: this.state.count +1 })
+  }
+  handleMinusButton=()=>{
+    this.setState({ count: this.state.count -1 })
+  }
+
+  return(){
+    return(
+      <React.Fragment>
+        <div>count:{ this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
+  
 }
 
-const User = (props) =>{
-return <div>Hi,I am {props.name},and{props.age} years old!</div>
-}
-
-User.propTypes={
-  name:PropTypes.string,
-  age:PropTypes.isRequired
-}
 
 export default App;
