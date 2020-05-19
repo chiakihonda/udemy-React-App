96ac61ba70e3900c9dcd6f9fca88decffd3fe02a
chiakihonda <chiaki.honnda@gmail.com>
2020-05-19 11:30:44 +0900

    impement events index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux"
-import { Provider} from "react-redux"
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-import reducer from "./reducers"
 import './index.css';
-import App from './components/App';
+import reducer from './reducers'
+import EventsIndex from './components/events_index';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventsIndex />
   </Provider>,
   document.getElementById('root')
 );
-
+registerServiceWorker();
